0f93d6f10aefdfbbfa6d2c89b98e0035a54cb038
Krivov Dmitrii <dmitriimanulov@gmail.com>
2026-09-19 13:54:30 +0700

    Добавлена функция purchasingPower и пункт меню

--- main.cpp
+++ main.cpp
@@ -11,15 +11,21 @@ double futurePrice(double price, double inf, int n) {
     return price * pow(1 + inf / 100.0, n);
 }
 
+// Покупательная способность: сумма/(1+инф/100)^n
+double purchasingPower(double sum, double inf, int n) {
+    return sum / pow(1 + inf / 100.0, n);
+}
+
 // ===== Главная функция: меню =====
 int main() {
     int choice;
-    double price, inf;
+    double price, inf, sum;
     int n;
 
     do {
         cout << "\n=== Вариант 98: расчёт инфляции ===\n";
         cout << "1. Будущая цена товара\n";
+        cout << "2. Покупательная способность\n";
         cout << "0. Выход\n";
         cout << "Выберите пункт: ";
         cin >> choice;
@@ -29,6 +35,11 @@ int main() {
                 cout << "Введите текущую цену, инфляцию (% в год) и срок (лет): ";
                 cin >> price >> inf >> n;
                 cout << "Будущая цена = " << futurePrice(price, inf, n) << "\n";
+                break;
+            case 2:
+                cout << "Введите сумму, инфляцию (% в год) и срок (лет): ";
+                cin >> sum >> inf >> n;
+                cout << "Покупательная способность = " << purchasingPower(sum, inf, n) << "\n";
                 break;
             case 0:
                 cout << "Работа завершена.\n";
